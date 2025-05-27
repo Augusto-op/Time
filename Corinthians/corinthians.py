fa15f95007263417e10bab232ad519c0219805b6
Augusto-op <augustoramossilva444@gmail.com>
2025-05-27 15:45:56 -0300

    melhor time

---
+++ Corinthians/corinthians.py
@@ -0,0 +1,8 @@
+time = input("Digite o nome do melhor time do mundo")
+
+while time != 'Corinthians':
+    time = input('Qual o melhor time  do mundo ?')
+    if time == "Corinthians":
+        print('Você acertou! Vai Corinthias')
+    else:
+        print("Tente nopvamente")
